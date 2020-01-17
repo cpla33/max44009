f85fc6298d2a981ac16afe4ea3a83462ba2bff40
cpla33 <40895125+cpla33@users.noreply.github.com>
2020-01-17 14:24:17 +0200

    Update max44009.py

--- max44009.py
+++ max44009.py
@@ -15,3 +15,14 @@ class MAX44009:
     def measure(self):
         self.i2c.writeto_mem(self.address,0x02,bytearray([0x40]))
         time.sleep(0.5)
+
+    # Read 2 bytes from the measured data storage register (0x03)
+    # Returns a bytes object with the data read
+    def read_value(self):
+        data = self.i2c.readfrom_mem(self.address, 0x03, 2)
+
+        exponent = (data[0] & 0xF0) >> 4
+        mantissa = ((data[0] & 0x0F) << 4) | (data[1] & 0x0F)
+        luminance = ((2 ** exponent) * mantissa) * 0.045
+
+        return luminance
